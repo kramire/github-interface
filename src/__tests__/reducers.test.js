53d93d8e645cd288b90713e146161fd0533e3f23
kramire <kramire@users.noreply.github.com>
2020-01-13 14:23:23 -0500

    feat: Wrote tests for reducers. Defined reducer via TDD.

---
+++ src/__tests__/reducers.test.js
@@ -0,0 +1,230 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-trailing-spaces */
+import gitHubApiReducer from '../redux/reducers';
+import * as types from '../redux/actionTypes';
+import testRepoData from '../testRepoData.json';
+import testIssueData from '../testIssueData.json';
+
+describe('Reducers Testing', () => {
+  describe('Set GitHub API Call Headers and Params', () => {
+    it('SET_API_KEY --> should update the apiKey given the passed string', () => {
+      // ARRANGE
+      const initialState = {
+        apiKey: '',
+      };
+      const action = {
+        type: types.SET_API_KEY,
+        data: '1234567890',
+      };
+  
+      const expectedState = {
+        apiKey: '1234567890',
+      };
+  
+      // ACT
+      const newState = gitHubApiReducer(initialState, action);
+  
+      // ASSERT
+      expect(newState).toEqual(expectedState);
+      expect(initialState).not.toEqual(newState);
+    });
+    it('SET_AUTHORIZATION_HEADER --> should update the authorization header with the api key', () => {
+      // ARRANGE
+      const initialState = {
+        headers: {
+          Authorization: '',
+        },
+      };
+      const action = {
+        type: types.SET_AUTHORIZATION_HEADER,
+        data: '1234567890',
+      };
+
+      const expectedState = {
+        headers: {
+          Authorization: 'token 1234567890',
+        },
+      };
+
+      // ACT
+      const newState = gitHubApiReducer(initialState, action);
+
+      // ASSERT
+      expect(newState).toEqual(expectedState);
+      expect(initialState).not.toEqual(newState);
+    });
+    it('SET_FILTER_PARAM --> should update the filter parameter given user selection', () => {
+      // ARRANGE
+      const initialState = {
+        issues: {
+          filter: '',
+        },
+      };
+      const action = {
+        type: types.SET_FILTER_PARAM,
+        data: 'mentioned',
+      };
+
+      const expectedState = {
+        issues: {
+          filter: 'mentioned',
+        },
+      };
+
+      // ACT
+      const newState = gitHubApiReducer(initialState, action);
+
+      // ASSERT
+      expect(newState).toEqual(expectedState);
+      expect(initialState).not.toEqual(newState);
+    });
+  });
+  describe('GitHub API call to get repositories', () => {
+    test('REQUEST_REPOSITORIES --> updates repositories isFetching property to true', () => {
+      // ARRANGE
+      const initialState = {
+        repositories: {
+          isFetching: false,
+        },
+      };
+      const action = {
+        type: types.REQUEST_REPOSITORIES,
+      };
+      const expectedState = {
+        repositories: {
+          isFetching: true,
+        },
+      };
+
+      // ACT
+      const newState = gitHubApiReducer(initialState, action);
+
+      // ASSERT
+      expect(newState).toEqual(expectedState);
+      expect(initialState).not.toEqual(newState);
+    });
+    test('SET_REPOSITORIES --> updates repositories items with response data', () => {
+      // ARRANGE
+      const initialState = {
+        repositories: {
+          isFetching: true,
+          items: [],
+        },
+      };
+      const action = {
+        type: types.SET_REPOSITORIES,
+        data: testRepoData,
+      };
+      const expectedState = {
+        repositories: {
+          isFetching: true,
+          items: testRepoData,
+        },
+      };
+
+      // ACT
+      const newState = gitHubApiReducer(initialState, action);
+
+      // ASSERT
+      expect(newState).toEqual(expectedState);
+      expect(initialState).not.toEqual(newState);
+    });
+    test('RECEIVED_REPOSITORIES --> updates repositories isFetching property to false', () => {
+      // ARRANGE
+      const initialState = {
+        repositories: {
+          isFetching: true,
+        },
+      };
+      const action = {
+        type: types.RECEIVED_REPOSITORIES,
+      };
+      const expectedState = {
+        repositories: {
+          isFetching: false,
+        },
+      };
+
+      // ACT
+      const newState = gitHubApiReducer(initialState, action);
+
+      // ASSERT
+      expect(newState).toEqual(expectedState);
+      expect(initialState).not.toEqual(newState);
+    });
+  });
+  describe('GitHub API call to get issues', () => {
+    test('REQUEST_ISSUES --> updates issues isFetching property to true', () => {
+      // ARRANGE
+      const initialState = {
+        issues: {
+          isFetching: false,
+        },
+      };
+      const action = {
+        type: types.REQUEST_ISSUES,
+      };
+      const expectedState = {
+        issues: {
+          isFetching: true,
+        },
+      };
+
+      // ACT
+      const newState = gitHubApiReducer(initialState, action);
+
+      // ASSERT
+      expect(newState).toEqual(expectedState);
+      expect(initialState).not.toEqual(newState);
+    });
+    test('SET_ISSUES --> updates issues items with response data', () => {
+      // ARRANGE
+      const initialState = {
+        issues: {
+          isFetching: true,
+          items: [],
+        },
+      };
+      const action = {
+        type: types.SET_ISSUES,
+        data: testIssueData,
+      };
+      const expectedState = {
+        issues: {
+          isFetching: true,
+          items: testIssueData,
+        },
+      };
+
+      // ACT
+      const newState = gitHubApiReducer(initialState, action);
+
+      // ASSERT
+      expect(newState).toEqual(expectedState);
+      expect(initialState).not.toEqual(newState);
+    });
+    test('RECEIVED_ISSUES --> updates issues isFetching property to false', () => {
+      // ARRANGE
+      const initialState = {
+        issues: {
+          isFetching: true,
+        },
+      };
+      const action = {
+        type: types.RECEIVED_ISSUES,
+      };
+      const expectedState = {
+        issues: {
+          isFetching: false,
+        },
+      };
+
+      // ACT
+      const newState = gitHubApiReducer(initialState, action);
+
+      // ASSERT
+      expect(newState).toEqual(expectedState);
+      expect(initialState).not.toEqual(newState);
+    });
+  });
+});
--- src/redux/reducers/gitHubApi.js
+++ src/redux/reducers/gitHubApi.js
@@ -4,19 +4,92 @@ const initialState = {
   apiKey: '',
   headers: {
     Accept: 'application/vnd.github.v3+json',
-    Authorization: 'token ',
+    Authorization: '',
   },
-  params: {
+  repositories: {
+    isFetching: false,
+    items: [],
+  },
+  issues: {
+    isFetching: false,
+    selectedRepository: '',
+    selectedUser: '',
+    items: [],
     filter: '',
   },
-  repositories: [],
-  selectedRepository: '',
-  selectedUser: '',
-  issues: [],
 };
 
 const gitHubApiReducer = (state = initialState, action) => {
   switch (action.type) {
+    case types.SET_API_KEY:
+      return {
+        ...state,
+        apiKey: action.data,
+      };
+    case types.SET_AUTHORIZATION_HEADER:
+      return {
+        ...state,
+        headers: {
+          ...state.headers,
+          Authorization: `token ${action.data}`,
+        },
+      };
+    case types.SET_FILTER_PARAM:
+      return {
+        ...state,
+        issues: {
+          ...state.issues,
+          filter: action.data,
+        },
+      };
+    case types.REQUEST_REPOSITORIES:
+      return {
+        ...state,
+        repositories: {
+          ...state.repositories,
+          isFetching: true,
+        },
+      };
+    case types.SET_REPOSITORIES:
+      return {
+        ...state,
+        repositories: {
+          ...state.repositories,
+          items: action.data,
+        },
+      };
+    case types.RECEIVED_REPOSITORIES:
+      return {
+        ...state,
+        repositories: {
+          ...state.repositories,
+          isFetching: false,
+        },
+      };
+    case types.REQUEST_ISSUES:
+      return {
+        ...state,
+        issues: {
+          ...state.issues,
+          isFetching: true,
+        },
+      };
+    case types.SET_ISSUES:
+      return {
+        ...state,
+        issues: {
+          ...state.issues,
+          items: action.data,
+        },
+      };
+    case types.RECEIVED_ISSUES:
+      return {
+        ...state,
+        issues: {
+          ...state.issues,
+          isFetching: false,
+        },
+      };
     default:
       return {
         ...state,
