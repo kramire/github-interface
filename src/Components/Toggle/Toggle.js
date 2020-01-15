2fc6c58db2bb553be1263c54e549ea7725472e55
kramire <kramire@users.noreply.github.com>
2020-01-15 10:30:12 -0500

    feat: Dropdown selection in Toggle persisted upon refresh.

--- src/Components/Toggle/Toggle.js
+++ src/Components/Toggle/Toggle.js
@@ -1,25 +1,30 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { setFilterParam, getIssues } from '../../redux/actions';
+import { capitalize } from '../../assests/utils';
 
-const Toggle = ({ setIssueFilter }) => {
-  const [filter, setFilter] = useState('');
+const Toggle = ({ stateFilter, setIssueFilter }) => {
+  const [filter, setFilter] = useState(stateFilter);
   function handleChange(e) {
     setIssueFilter(e.target.value);
   }
+  const filterOptions = ["assigned", "created", "mentioned", "subscribed", "all"];
   return (
     <div>
-      <label>Filter:
-        <select onChange={handleChange}>
-          <option selected value="assigned">Assigned</option>
-          <option value="created">Created</option>
-          <option value="mentioned">Mentioned</option>
-          <option value="subscribed">Subscribed</option>
-          <option value="all">All</option>
+      <label>
+        Filter:
+        <select onChange={handleChange} value={stateFilter}>
+          {filterOptions.map(el => <option value={el}>{capitalize(el)}</option>)}
         </select>
       </label>
     </div>
   );
+};
+
+const mapStateToProps = state => {
+  return {
+    stateFilter: state.issues.filter,
+  };
 };
 
 const mapDispatchToProps = dispatch => {
@@ -31,4 +36,4 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default connect(null, mapDispatchToProps)(Toggle);
+export default connect(mapStateToProps, mapDispatchToProps)(Toggle);
--- src/assests/utils.js
+++ src/assests/utils.js
@@ -1,3 +1,52 @@
-Unstaged changes after reset:
-M	src/assests/utils.js
-D	src/utils.js
+// given a date as a string, return it with the format dd/mm/yyyy
+function formatEuropeanDate(dateString) {
+  const date = new Date(dateString);
+  return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+}
+
+// given a historical date, return colloquially how long "ago"
+function calcHowLongAgo(dateString) {
+  const date = new Date(dateString);
+  const now = Date.now();
+  const hoursAgo = Math.floor(now - date) / 36e5;
+  const daysAgo = Math.floor(hoursAgo / 24);
+  const monthsAgo = Math.floor(daysAgo / 30);
+
+  if (monthsAgo > 1) return `${monthsAgo} months ago`;
+  if (monthsAgo === 1) return `1 month ago`;
+  if (daysAgo > 1) return `${daysAgo} days ago`;
+  if (daysAgo === 1) return `1 day ago`;
+  if (hoursAgo > 1) return `${hoursAgo} hours ago`;
+  if (hoursAgo === 1) return `1 hour ago`;
+  return `Under an hour ago.`;
+}
+
+// save the state in localStorage in order to persist the data
+function saveStateLocally(state) {
+  const stringifiedState = JSON.stringify(state);
+  localStorage.setItem('state', stringifiedState);
+}
+
+// load the state from localStorage
+function loadLocalState() {
+  try {
+    const previousState = localStorage.getItem('state') || undefined;
+    return JSON.parse(previousState);
+  } 
+  catch (err) {
+    return undefined;
+  }
+}
+
+// capitalize a string
+function capitalize(string) {
+  return string[0].toUpperCase() + string.slice(1,);
+}
+
+module.exports = {
+  formatEuropeanDate,
+  calcHowLongAgo,
+  saveStateLocally,
+  loadLocalState,
+  capitalize,
+};
